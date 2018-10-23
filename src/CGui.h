f7515017ff3de1023548d654acfd559352f606df
Jiri Ulrich <jiri.ulrich@fel.cvut.cz>
2018-10-23 17:21:35 +0200

    Memory leaks fixed. However there might be leaks caused by XLib, at least it looks like that in valgrind output.

--- src/CGui.h
+++ src/CGui.h
@@ -9,27 +9,29 @@
 
 class CGui
 {
-public:
-  CGui(int wi,int he,int scale, const char* font_path);
-  ~CGui();
+    public:
 
-  void drawImage(CRawImage* image);
-  void drawStats(int x,int y,STrackedObject o);
-  void drawTimeStats(int evalTime,int numBots);
-  void guideCalibration(int calibNum,float a,float b);
-  void clearStats();
-  void saveScreen(int a = -1);
-  void update();
-  void drawEllipse(SSegment s,STrackedObject t);
-  void drawLine(float sx1,float sx2,float sy1,float sy2);
-  void displayHelp(bool extended);
-  void drawStats(int x,int y,STrackedObject o, bool D2);
+        CGui(int wi,int he,int scale, const char* font_path);
+        ~CGui();
 
-private:
-  SDL_Surface *screen;
-  int width,height,scale;
-  TTF_Font *smallFont;
-  int averageTime,maxTime,numStats,num;	
+        void drawImage(CRawImage* image);
+        void drawStats(int x,int y,STrackedObject o);
+        void drawTimeStats(int evalTime,int numBots);
+        void guideCalibration(int calibNum,float a,float b);
+        void clearStats();
+        void saveScreen(int a = -1);
+        void update();
+        void drawEllipse(SSegment s,STrackedObject t);
+        void drawLine(float sx1,float sx2,float sy1,float sy2);
+        void displayHelp(bool extended);
+        void drawStats(int x,int y,STrackedObject o, bool D2);
+
+    private:
+
+        SDL_Surface *screen;
+        int width,height,scale;
+        TTF_Font *smallFont;
+        int averageTime,maxTime,numStats,num;	
 };
 
 #endif
